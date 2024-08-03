04658afdb9e74a1a53e859bbace223ce741e0433
Nikhil <nikhilpriyadarshan1234@gmail.com>
2024-08-03 19:21:30 +0530

    .env

--- server/Database/mysql.js
+++ server/Database/mysql.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Replace with your MySQL username
-  password: 'admin123', // Replace with your MySQL password
-  database: 'seat_selection'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  charset: 'utf8mb4',
+  connectTimeout: 10000,
+  ssl: {
+    rejectUnauthorized: false, // Disable SSL certificate verification
+  }
 });
 
 db.connect(err => {
